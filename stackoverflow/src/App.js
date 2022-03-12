b50d704e9cd80c26893b9510ccd9216eb304646a
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-03-12 23:33:11 +0100

    files

--- stackoverflow/src/App.js
+++ stackoverflow/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import setAuthToken from './redux/auth/auth.utils';
+import {loadUser} from './redux/auth/auth.actions';
+import RoutesTree from './RoutesTree';
+
+import Header from './components/Header/Header.component';
+import Alert from './components/Alert/Alert.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <div className='App'>
+        <Header />
+        <Alert />
+        <RoutesTree />
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
